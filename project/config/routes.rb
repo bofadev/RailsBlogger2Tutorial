d77afde7bee254c81b88a382d4874bb69bcc4107
bofadev <brownwdev@gmail.com>
2017-12-02 14:44:09 -0500

    authentication
    + added sorcery authentication
    + added login/logout
    + wrapped up create/destroy/edit functionality in logged in guards

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "articles#index"
+	root to: "articles#index"
 
-  resources :articles
+	resources :articles
+	resources :articles do
+		resources :comments
+	end
 
-  resources :articles do
-  	resources :comments
-  end
+	resources :tags
 
-  resources :tags
+	resources :authors
+
+	# authentication
+	resources :author_sessions, only: [ :new, :create, :destroy ]
+	get 'login'  => 'author_sessions#new'
+	get 'logout' => 'author_sessions#destroy'
+
 end
